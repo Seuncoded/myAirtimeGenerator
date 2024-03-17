6123a768c68e65fdd4494e482284f6a818c33ed9
seuncoded <seuncodedmedia@gmail.com>
2024-03-17 22:56:35 +0100

    Done with Html and Js

--- main.js
+++ main.js
@@ -1,25 +1,38 @@
 let cards = document.getElementById('airtimeDisplayScreen');
 let newCard = Math.floor(Math.random() * 10000000000000000)
-console.log(newCard);
+let network = document.getElementById('network')
+let amounts = document.getElementById('amount')
+let airtime ;
 
 function pinGenerate() {
-    cards.innerHTML = Math.floor(Math.random(newCard) * 100000000000000);
+    // cards.innerHTML = Math.floor(Math.random(newCard) * 100000000000000);
+    if (network.value == "" || amounts.value ==""){
+        alert("Select Amounts and Your Preffered Services Provider")
+    }else{
+       airtime = Math.floor(Math.random(newCard) * 100000000000000);
+       cards.innerHTML = airtime;
+    }
 }
+
+
 
 
 
 let cardEnter = document.getElementById('loadCard')
-let cardType = ['mtn', 'glo', 'airtel', '9mobile']
-
-let cardNetwork = [0]
-
 
 function loadAirtime() {
 
-    if (cardEnter.value == cards.innerHTML){
-        alert('Recharge Was Successful')
-    } else {
-        alert('Try gain')
+    if (cardEnter.value == ""){
+        alert("Fill the Input")
+    } 
+    else if (cardEnter.value.startsWith("*311*") && cardEnter.value .includes(airtime) && cardEnter.value.endsWith("#") ){
+        alert(` you have successfully recharge  ${network.value}  ${amounts.value} airtime`)
+    }else{
+        alert('Incorret Pin')
     }
+    console.log(network .value);
+    console.log(amounts.value);
+
 
 }
+
